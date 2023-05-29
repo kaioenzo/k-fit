9b87daa249b53395e6e0f6ce356e36b70f6a4e3e
Kaio Enzo <kaioenzobr@gmail.com>
2023-05-29 20:01:07 -0300

    feat: update bottomNavigation

--- src/components/BottomNavigation/BottomNavigation.svelte
+++ src/components/BottomNavigation/BottomNavigation.svelte
@@ -6,10 +6,10 @@
 
 {#if $user}
   <footer>
-    <div class="container-row justify-between">
+    <div class="container-row space-around">
       <button
         on:click={() => {
-          push("/home")
+          push("/")
             .then(async () => await tick())
             .catch((e) => {
               throw e;
@@ -17,14 +17,8 @@
         }}
       >
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-            ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M32 0C23.1 0 14.6 3.7 8.6 10.2S-.6 25.4 .1 34.3L28.9 437.7c3 41.9 37.8 74.3 79.8 74.3H275.3c42 0 76.8-32.4 79.8-74.3L383.9 34.3c.6-8.9-2.4-17.6-8.5-24.1S360.9 0 352 0H32zM73 156.5L66.4 64H317.6L311 156.5l-24.2 12.1c-19.4 9.7-42.2 9.7-61.6 0c-20.9-10.4-45.5-10.4-66.4 0c-19.4 9.7-42.2 9.7-61.6 0L73 156.5z"
-            /></svg
-          >
-          <h6>Água</h6>
+          <span class="material-symbols-outlined"> home </span>
+          <h6>Home</h6>
         </div>
       </button>
       <button
@@ -37,19 +31,13 @@
         }}
       >
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-            ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M32 0C23.1 0 14.6 3.7 8.6 10.2S-.6 25.4 .1 34.3L28.9 437.7c3 41.9 37.8 74.3 79.8 74.3H275.3c42 0 76.8-32.4 79.8-74.3L383.9 34.3c.6-8.9-2.4-17.6-8.5-24.1S360.9 0 352 0H32zM73 156.5L66.4 64H317.6L311 156.5l-24.2 12.1c-19.4 9.7-42.2 9.7-61.6 0c-20.9-10.4-45.5-10.4-66.4 0c-19.4 9.7-42.2 9.7-61.6 0L73 156.5z"
-            /></svg
-          >
+          <span class="material-symbols-outlined"> water_loss </span>
           <h6>Água</h6>
         </div>
       </button>
       <button
         on:click={() => {
-          push("/home")
+          push("/breaks")
             .then(async () => await tick())
             .catch((e) => {
               throw e;
@@ -57,54 +45,8 @@
         }}
       >
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-            ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M32 0C23.1 0 14.6 3.7 8.6 10.2S-.6 25.4 .1 34.3L28.9 437.7c3 41.9 37.8 74.3 79.8 74.3H275.3c42 0 76.8-32.4 79.8-74.3L383.9 34.3c.6-8.9-2.4-17.6-8.5-24.1S360.9 0 352 0H32zM73 156.5L66.4 64H317.6L311 156.5l-24.2 12.1c-19.4 9.7-42.2 9.7-61.6 0c-20.9-10.4-45.5-10.4-66.4 0c-19.4 9.7-42.2 9.7-61.6 0L73 156.5z"
-            /></svg
-          >
-          <h6>Água</h6>
-        </div>
-      </button>
-      <button
-        on:click={() => {
-          push("/home")
-            .then(async () => await tick())
-            .catch((e) => {
-              throw e;
-            });
-        }}
-      >
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-            ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M32 0C23.1 0 14.6 3.7 8.6 10.2S-.6 25.4 .1 34.3L28.9 437.7c3 41.9 37.8 74.3 79.8 74.3H275.3c42 0 76.8-32.4 79.8-74.3L383.9 34.3c.6-8.9-2.4-17.6-8.5-24.1S360.9 0 352 0H32zM73 156.5L66.4 64H317.6L311 156.5l-24.2 12.1c-19.4 9.7-42.2 9.7-61.6 0c-20.9-10.4-45.5-10.4-66.4 0c-19.4 9.7-42.2 9.7-61.6 0L73 156.5z"
-            /></svg
-          >
-          <h6>Água</h6>
-        </div>
-      </button>
-      <button
-        on:click={() => {
-          push("/home")
-            .then(async () => await tick())
-            .catch((e) => {
-              throw e;
-            });
-        }}
-      >
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 384 512"
-            ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-              d="M32 0C23.1 0 14.6 3.7 8.6 10.2S-.6 25.4 .1 34.3L28.9 437.7c3 41.9 37.8 74.3 79.8 74.3H275.3c42 0 76.8-32.4 79.8-74.3L383.9 34.3c.6-8.9-2.4-17.6-8.5-24.1S360.9 0 352 0H32zM73 156.5L66.4 64H317.6L311 156.5l-24.2 12.1c-19.4 9.7-42.2 9.7-61.6 0c-20.9-10.4-45.5-10.4-66.4 0c-19.4 9.7-42.2 9.7-61.6 0L73 156.5z"
-            /></svg
-          >
-          <h6>Água</h6>
+          <span class="material-symbols-outlined"> directions_walk </span>
+          <h6>Breaks</h6>
         </div>
       </button>
     </div>
@@ -121,6 +63,9 @@
     position: fixed;
     bottom: 0;
     width: 97%;
+  }
+  span {
+    font-size: 2.2rem;
   }
 
   button {
